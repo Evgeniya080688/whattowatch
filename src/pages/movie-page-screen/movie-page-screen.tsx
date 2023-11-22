dd166eab0f6e90333f82c2d7a57ccf6ba90ec01d
PaladinaEug <paladina.em1@kvt.su>
2023-11-22 17:29:11 +0300

    3.3. Обновит ссылку для логотипа.

--- src/pages/movie-page-screen/movie-page-screen.tsx
+++ src/pages/movie-page-screen/movie-page-screen.tsx
@@ -1,3 +1,4 @@
+import Logo from '../../components/logo/logo';
 type MyListScreenProps = {
   test: string;
 }
@@ -14,13 +15,7 @@ function MoviePageScreen({test}: MyListScreenProps): JSX.Element {
           <h1 className="visually-hidden">WTW</h1>
 
           <header className="page-header film-card__head">
-            <div className="logo">
-              <a href="main.html" className="logo__link">
-                <span className="logo__letter logo__letter--1">W</span>
-                <span className="logo__letter logo__letter--2">T</span>
-                <span className="logo__letter logo__letter--3">W</span>
-              </a>
-            </div>
+            <Logo />
 
             <ul className="user-block">
               <li className="user-block__item">
