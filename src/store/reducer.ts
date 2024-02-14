b57c2898c3006e61768e3ff9ff0bace2f781d488
PaladinaEug <paladina.em1@kvt.su>
2024-02-14 13:47:06 +0300

    6.3. Напишет условие для фильтрации фильмов по жанру

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -3,7 +3,7 @@ import {changeGenge, getFilmsByGenre} from './action';
 import {films} from './../mocks/films';
 
 const initialState = {
-  genre: 'All',
+  genre: 'All genres',
   filmsList: films,
 };
 
@@ -13,7 +13,8 @@ const reducer = createReducer(initialState, (builder) => {
       state.genre = action.payload;
     })
     .addCase(getFilmsByGenre,(state, action) => {
-      state.filmsList = state.filmsList.filter((filmItem) => filmItem.genre === action.payload);
+      state.filmsList = state.filmsList.filter((filmItem) =>
+        (action.payload !== 'All genres') ? filmItem.genre === action.payload : true );
     });
 });
 
