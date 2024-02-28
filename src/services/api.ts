7ccf8c9a60647f3c3b6b9551f4926876982ab899
PaladinaEug <paladina.em1@kvt.su>
2024-02-28 17:33:04 +0300

    7.3. Добавит модуль для работы с токеном.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios, {AxiosInstance} from 'axios';
 
-const BACKEND_URL = 'https://13.design.pages.academy/guess-melody';
+const BACKEND_URL = 'https://10.react.htmlacademy.pro/wtw';
 const REQUEST_TIMEOUT = 5000;
 
 export const createAPI = (): AxiosInstance => {
